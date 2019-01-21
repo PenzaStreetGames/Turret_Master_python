2a6f8ee61634d461e9046b521002694a2e01d72b
medal <webformedal@gmail.com>
2019-01-21 23:30:57 +0300

    Разработка начата.

--- start_menu.py
+++ start_menu.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 
-size = width, height = (500, 500)
+size = WIDTH, HEIGHT = (800, 600)
 v = 50  # пикселей в секунду
 fps = 40
 
@@ -16,47 +16,53 @@ def load_image(name, colorkey=None):
         raise SystemExit(message)
 
 
-class MenuItem(pygame.sprite.Sprite):
-    def __init__(self, group, coords):
+def get_font(size, bold=False):
+    return pygame.font.Font(os.path.join("data", "fonts", 'PSG_Font.ttf'), size)
+
+
+class Title(pygame.sprite.Sprite):
+    def __init__(self, group, width, height, x, y, text=None):
         super().__init__(group)
+        group.add(self)
+        self.width = width
+        self.height = height
+        self.x, self.y = x, y
+        self.image = pygame.Surface((self.width, self.height))
+        pygame.draw.rect(self.image, pygame.Color('#BFBECD'), (0, 0, self.width, self.height), 0)
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-    def update(self):
-        pass
 
-    def get_event(self, event):
-        pass
+    def set_text(self, text, text_x, text_y, size=135, bold=True):
+        font = get_font(size, bold=True)
+        text = font.render(text, 1, pygame.Color("#323232"))
+        self.image.blit(text, (text_x, text_y))
 
+
+def start_window():
+    title_width, title_height = 400, 80
+    title = Title(all_sprites, title_width, title_height, WIDTH // 2 - title_width // 2, 1)
+    title.set_text("Мастер турелей", title.x // 7, title.y + 10)
+    auth_header_x, auth_header_y = 360, 60,
+    auth_header = Title(all_sprites, auth_header_x, auth_header_y, WIDTH // 2 - 360 // 2, HEIGHT // 4, "Мастер турелей")
+    auth_header.set_text("fgs", auth_header.x, auth_header.y)
+    # auth = WindowAuth(all_sprites, WIDTH, HEIGHT)
 
 
 
 pygame.init()
-
-
-
 screen = pygame.display.set_mode(size)
 running = True
 screen.fill(pygame.Color("#9995BD"))
-
+all_sprites = pygame.sprite.Group()
+start_window()
 while running:
-    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                Platform(platforms_group, event.pos)
-            if event.button == 3:
-                if not player:
-                    player = Player(main_group, event.pos)
-                else:
-                    player.set_coords(event.pos)
-        if event.type == pygame.KEYDOWN:
-            key = pygame.key.get_pressed().index(1)
-            if key == 276:
-                player.set_coords((player.rect.x - 10, player.rect.y))
-            elif key == 275:
-                player.set_coords((player.rect.x + 10, player.rect.y))
+    all_sprites.update()
+    all_sprites.draw(screen)
+
 
 
     pygame.display.flip()
