7048b35506661ca7b3845363307cc57ced8f8ef7
Gamba232 <parassitn@mail.ru>
2019-02-08 23:11:24 +0300

    После каждого перехода в другую ветку после изменений необходим коммит. Что за идиотизм.

---
+++ FileLoadManager.py
@@ -0,0 +1,28 @@
+import json
+
+
+def load_json_file(name, formating=True):
+    with open(name, mode="r", encoding="utf-8") as file:
+        data_file = json.loads(file.read())
+    if formating:
+        text = ["    ".join([row[0].ljust(22, " "), row[1]]) for row in data_file.items()]
+        return text
+    return data_file
+
+
+def save_json_file(name, arg):
+    with open(name, mode="w", encoding="utf-8") as file:
+        file.write(json.dumps(arg))
+
+
+def load_data_file(name):
+    with open(name, mode="r", encoding="utf-8") as file:
+        text = file.read().split("\n")
+    return text
+
+
+def formating(arr):
+    if not arr:
+        return ""
+    text = ["    ".join([row[0].ljust(22, " "), str(row[1])]) for row in arr]
+    return text
